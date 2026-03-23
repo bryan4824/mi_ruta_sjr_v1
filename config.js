a98e9d0337ac6589d77dfcdbee017c1dcd3bf514
Bryan de Jesus <santiagobryan770@gmail.com>
2026-03-23 11:52:39 -0600

    update file config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 config(); // Esto carga las variables del archivo .env en local
 
+export const PORT = process.env.PORT || 3000;
+
 export const API_BASE = process.env.API_BASE;
 export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY;
 export const EMAIL_USER = process.env.EMAIL_USER;
